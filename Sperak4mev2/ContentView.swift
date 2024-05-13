f60f79729c482e2d52c13a97748d7521f379c53e
wengerp <info@wepcom.ch>
2024-05-13 19:32:34 +0200

    Initial Commit

---
+++ Sperak4mev2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sperak4mev2
+//
+//  Created by Patrick Wenger on 13.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sperak4mev2/Sperak4mev2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sperak4mev2App.swift
+//  Sperak4mev2
+//
+//  Created by Patrick Wenger on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sperak4mev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
